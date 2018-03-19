72d7edb6433a0c289beae25e0b0a72fc7f654790
Dodo <s224965@studenti.polito.it>
2018-03-19 15:09:32 +0100

    Dovrebbe essere completo questo ora

--- Ex1.py
+++ Ex1.py
@@ -1,1 +1,8 @@
-print("test")
+print("Insert a number: ",end='')
+num1=input()
+print("Insert another number: ",end='')
+num2=input()
+print("La loro somma fa: {}".format(int(num1) + int(num2) ))
+
+#NB: modificare il end='carattere' modifica solo quello
+#di questo print
